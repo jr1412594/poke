9cf133edc02a2e2ef60e1ca129b803ad1372e514
jr1412594 <jr.medina1412@gmail.com>
2021-09-12 17:40:54 -0600

    repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import Container from './components/Container';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const baseUrl = 'https://pokeapi.co/api/v2/pokemon?offset=100&limit=100'
+
+class App extends Component {
+
+  state = {
+    pokemon : []
+  }
+
+  componentDidMount(){
+    fetch(baseUrl)
+      .then(response => response.json())
+      .then(pokemon => this.setState({ pokemon : pokemon.results }))
+    }
+    
+    render() {
+      return (
+        <div className="App">
+        
+        <h1>{console.log('this is my pokemon key',this.state.pokemon)}</h1>
+        hello!
+        <Container pokemon={ this.state.pokemon }/>
+      </div>
+    );
+  }
 }
 
 export default App;
