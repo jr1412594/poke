eb40cb5a40da3fda0675ea8e9aa0b6126b0b2a09
jr1412594 <jr.medina1412@gmail.com>
2021-09-15 21:32:58 -0600

    nested data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import './App.css';
-import Container from './components/Container';
+// import Container from './components/Container';
 
-const baseUrl = 'https://pokeapi.co/api/v2/pokemon?offset=100&limit=100'
+const baseUrl = 'https://pokeapi.co/api/v2/pokemon?limit=151'
 
 class App extends Component {
 
@@ -10,19 +10,36 @@ class App extends Component {
     pokemon : []
   }
 
+  fetchPokemonData(pokemon) {
+    let url = pokemon.url
+
+    fetch(url)
+    .then(response => response.json())
+    .then(function(pokeData) {
+      console.log(pokeData)
+    })
+  }
   componentDidMount(){
     fetch(baseUrl)
-      .then(response => response.json())
-      .then(pokemon => this.setState({ pokemon : pokemon.results }))
-    }
+    .then(response => response.json())
+    .then(function(allPokemon) {
+      allPokemon.results.forEach(pokemon => {
+        console.log(pokemon.url)
+        fetch(pokemon.url)
+        .then(response => response.json())
+        .then(result => this.setState({pokemon : [...this.state.pokemon, result]}))
+      })
+    })
+  }
+  
     
     render() {
       return (
         <div className="App">
         
-        <h1>{console.log('this is my pokemon key',this.state.pokemon)}</h1>
+        {/* <h1>{console.log('this is my pokemon key',this.state.pokemon)}</h1> */}
         hello!
-        <Container pokemon={ this.state.pokemon }/>
+        {/* <Container pokemon={ this.state.pokemon }/> */}
       </div>
     );
   }
